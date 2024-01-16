68331a2660ef3b68d22b5f429909cadeb5b5b6e4
switchII <luoandon@gmail.com>
2024-01-16 21:28:16 +0800

    feat:完善和更新插件

--- alinesno-business-analyst-agent/src/main/java/com/alinesno/infra/smart/assistant/plugin/demo/BusinessAnalystDemoPluginChainExpert.java
+++ alinesno-business-analyst-agent/src/main/java/com/alinesno/infra/smart/assistant/plugin/demo/BusinessAnalystDemoPluginChainExpert.java
@@ -3,11 +3,14 @@ package com.alinesno.infra.smart.assistant.plugin.demo;
 import com.alinesno.infra.smart.assistant.im.dto.NoticeDto;
 import com.alinesno.infra.smart.assistant.role.PlatformExpert;
 import com.alinesno.infra.smart.assistant.role.context.RoleChainContext;
+import com.alinesno.infra.smart.assistant.role.utils.YamlUtils;
 import com.yomahub.liteflow.annotation.LiteflowComponent;
 import com.yomahub.liteflow.core.NodeComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,15 +18,18 @@ import java.util.Map;
  */
 @Slf4j
 @Component
-public class DemoPluginChainExpert extends PlatformExpert {
+public class BusinessAnalystDemoPluginChainExpert extends PlatformExpert {
 
     private static final String promptId = "0GSheQ31" ;
+    private List<String> resultMap ;
 
-    @LiteflowComponent(value = "DemoPlugin_a" , name="插件测试执行节点A")
+    @LiteflowComponent(value = "BADemoPlugin_a" , name="插件测试执行节点A")
     public class ACmp extends NodeComponent {
 
         @Override
         public void process() {
+            resultMap = new ArrayList<>() ;
+
             System.out.println("ACmp executed!");
 
             // 获取上下文
@@ -42,10 +48,13 @@ public class DemoPluginChainExpert extends PlatformExpert {
             log.debug("businessId = {}" , businessId);
             log.debug("noticeDto = {}" , noticeDto);
 
+            resultMap.add(businessId) ;
+            resultMap.add(roleContext.getAssistantYamlContent()) ;
+
         }
     }
 
-    @LiteflowComponent(value = "DemoPlugin_b" , name="插件测试执行节点B")
+    @LiteflowComponent(value = "BADemoPlugin_b" , name="插件测试执行节点B")
     public class BCmp extends NodeComponent {
 
         @Override
@@ -55,12 +64,20 @@ public class DemoPluginChainExpert extends PlatformExpert {
 
     }
 
-    @LiteflowComponent(value = "DemoPlugin_c" , name="插件测试执行节点C")
+    @LiteflowComponent(value = "BADemoPlugin_c" , name="插件测试执行节点C")
     public class CCmp extends NodeComponent {
 
         @Override
         public void process() {
             System.out.println("CCmp executed!");
+            RoleChainContext roleContext = this.getContextBean(RoleChainContext.class) ;
+            String businessId = roleContext.getBusinessId() ; // 获取到业务Id
+
+            log.debug("YamlUtils.mergedYamlList(resultMap) = \r\n{}" , YamlUtils.mergedYamlList(resultMap));
+
+            // 将聚合生成的内容保存到内容数据库中
+            saveToBusinessResult(businessId , YamlUtils.mergedYamlList(resultMap)) ;
+
         }
 
     }
